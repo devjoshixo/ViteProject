40258a89a66dbb6eebf61f04d6bed00b917a8516
devjoshixo <djoshi923@gmail.com>
2023-07-25 12:18:58 +0530

    Final Push

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import SecondPage from './pages/SecondPage';
+import ErrorPage from './pages/ErrorPage';
 import LoginContext from './context/Login/LoginContext';
 import { useContext } from 'react';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
@@ -15,6 +16,9 @@ function App() {
       </Route>
       <Route path='/next-page'>
         {ctx.loggedIn ? <SecondPage /> : <Redirect to='/' />}
+      </Route>
+      <Route path='*'>
+        <ErrorPage />
       </Route>
     </Switch>
   );
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>404 page not found on my site</h1>
+      <a href='/'>Go to Home</a>
+    </div>
+  );
+};
+
+export default ErrorPage;
